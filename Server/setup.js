353d760c87aabbdf8c68d197dfc435fe0acac11b
p3nt4 <louis.nacfr@gmail.com>
2020-04-10 13:12:29 -0400

    Updated Installation Process

---
+++ Server/setup.js
@@ -0,0 +1,109 @@
+const fs = require('fs');
+const crypto = require('crypto');
+const inquirer = require('inquirer');
+const MongoClient = require('mongodb').MongoClient;
+const bcrypt = require('bcryptjs');
+
+const BCRYPT_WORK_FACTOR_BASE = 12;
+const BCRYPT_DATE_BASE = 1483228800000;
+const BCRYPT_WORK_INCREASE_INTERVAL = 47300000000;
+
+function hasher (password) {
+  return new Promise((resolve, reject) => {
+    let BCRYPT_CURRENT_DATE = new Date().getTime();
+    let BCRYPT_WORK_INCREASE = Math.max(0, Math.floor((BCRYPT_CURRENT_DATE - BCRYPT_DATE_BASE) / BCRYPT_WORK_INCREASE_INTERVAL));
+    let BCRYPT_WORK_FACTOR = Math.min(19, BCRYPT_WORK_FACTOR_BASE + BCRYPT_WORK_INCREASE);
+    bcrypt.genSalt(BCRYPT_WORK_FACTOR, function (error, salt) {
+      if (error) {
+        return reject(error);
+      }
+
+      bcrypt.hash(password, salt, function (error, hashedPassword) {
+        if (error) {
+          return reject(error);
+        }
+
+        resolve(hashedPassword);
+      });
+    });
+  });
+};
+
+const sourceFileName = './config/default.json';
+const destinationFileName = './config/production.json';
+const sourceFile = require(sourceFileName);
+
+nuagesUser = {}
+
+var nuagesUserQuestions = [
+  {
+    type: 'input',
+    message: 'Enter the username of the Nuages user:  ',
+    name: 'username',
+  },
+  {
+    type: 'password',
+    message: 'Enter the password of the Nuages user:  ',
+    name: 'password1',
+    mask: '*',
+  },
+  {
+    type: 'password',
+    message: 'Retype the password of the Nuages user: ',
+    name: 'password2',
+    mask: '*',
+  }
+];
+
+function promptNuagesUser() {
+  inquirer.prompt(nuagesUserQuestions).then(answers => {
+    if (answers.password1 != answers.password2) {
+      console.log("  Passwords do not match!")
+      promptNuagesUser();
+    } else {
+      nuagesUser = answers;
+      hasher(nuagesUser.password1).then((hash)=>{
+        nuagesUser.password = hash;
+        promptMongoDB();
+      });
+    }
+  });
+}
+
+function promptMongoDB() {
+  inquirer.prompt([{
+    type: 'input',
+    message: 'Enter the mongodb connection string:  ',
+    name: 'mongodb',
+    default: 'mongodb://localhost:27017/nuages_c_2'
+  }]).then(answers => {
+    MongoClient.connect(answers.mongodb, { useNewUrlParser: true }, function(err, db) {
+        if (err){
+          console.log("  Error connecting to MongoDB: " + err.message);
+          promptMongoDB();
+        }else{
+          var dbo = db.db(db.s.options.dbName);
+          dbo.collection('users').insertOne({
+              email: nuagesUser.username,
+              password: nuagesUser.password
+          },function (error, response) {
+            if(err) {
+              console.log("  Error creating the Nuages user: " + err.message);
+              promptMongoDB();
+            } else {
+                sourceFile.mongodb = answers.mongodb;
+                sourceFile.authentication.secret = crypto.randomBytes(256).toString('hex');
+                fs.writeFile(destinationFileName, JSON.stringify(sourceFile, null, 2), function writeJSON(err) {
+                  if (err) return console.log(err); else process.exit(0);
+                });
+                //process.exit(0);
+              // return 
+            }
+        });
+        }
+    });
+  });
+}
+
+promptNuagesUser();
+
--- Server/setup.sh
+++ Server/setup.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-#command -v mongod >/dev/null 2>&1 || { echo >&2 "mongodb required.  Aborting."; exit 1; }
 command -v npm >/dev/null 2>&1 || { echo >&2 "npm required.  Aborting."; exit 1; }
 command -v node >/dev/null 2>&1 || { echo >&2 "node required.  Aborting."; exit 1; }
 
 printf "\n\nInstalling local npm dependencies\n";
-npm install 2&>1 >/dev/null;
+npm install 2>&1 >/dev/null;
 
 if [ $? -eq 0 ]; then
     echo "Done!"
@@ -13,106 +12,12 @@ else
     exit 1
 fi
 
-printf "\n\nInstalling feathers cli\n";
-npm install @feathersjs/cli  2&>1 >/dev/null;
-
+printf "Configuring Nuages:\n";
+node ./setup.js
 if [ $? -eq 0 ]; then
-    echo "Done!"
+    echo "Success!";
 else
-    echo "FAILED !"
-    exit 1
-fi
-
-command -v ./node_modules/@feathersjs/cli/bin/feathers.js >/dev/null 2>&1 || { printf >&2 "\n\nFeathers-cli install failed.  Aborting."; exit 1; }
-
-printf "\n\nSetting up database, select mongodb and enter connection string:\n"
-./node_modules/@feathersjs/cli/bin/feathers.js generate connection;
-
-if [ $? -eq 0 ]; then
-    echo "Done!"
-else
-    echo "FAILED !"
-    exit 1
-fi
-
-
-printf "\n\nAdding support for gridfs...\n";
-cp src/mongodb.js.grid src/mongodb.js
-
-if [ $? -eq 0 ]; then
-    echo "Done!"
-else
-    echo "FAILED !"
-    exit 1
-fi
-
-printf "\n\nSetting up authentication, select local and users:\n";
-./node_modules/@feathersjs/cli/bin/feathers.js generate authentication;
-
-if [ $? -eq 0 ]; then
-    echo "Done!"
-else
-    echo "FAILED !"
-    exit 1
-fi
-
-printf "\n\nSetting up for production mode...\n";
-cp config/default.json config/production.json
-
-if [ $? -eq 0 ]; then
-    echo "Done!";
-else
-    echo "FAILED !";
-    exit 1;
-fi
-
-printf "\n\nWhat should be the username of the Nuages user:\n";
-read user;
-
-printf "\n\nWhat should be the password of the Nuages user:\n";
-read password;
-
-printf "\n\nDisabling authentication";
-cp src/services/users/users.hooks.js.noauth src/services/users/users.hooks.js
-
-printf "\n\nStarting the server in the background\n";
-node src/ &
-
-sleep 5;
-
-printf "\n\nCreating the user\n";
-curl 'http://localhost:3030/users/' \
--H 'Content-Type: application/json' \
---data-binary @<(cat <<EOF
-{
-  "email": "$user",
-  "password": "$password"
-  }
-EOF
-)
-
-if [ $? -eq 0 ]; then
-    echo "Success!"
-else
-    echo "FAILED ! Doesnt look like the user was created"
-fi
-
-printf "\n\nEnabling authentication\n";
-cp src/services/users/users.hooks.js.auth src/services/users/users.hooks.js
-
-if [ $? -eq 0 ]; then
-    echo "Done!"
-else
-    echo "FAILED ! THE SERVER MIGHT NOT HAVE AUTHENTICATION ENABLED";
-fi
-
-printf "\n\n Killing the node server...\n\n"
-kill $(pgrep -f "node src/")
-
-if [ $? -eq 0 ]; then
-    echo "Done!";
-else
-    echo "FAILED ! The Nuages server might still be running without authentication, make sure to restart it";
+    echo "FAILED!";
 	exit 1;
 fi
 
