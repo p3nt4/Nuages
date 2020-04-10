edb6516a2d8306eddfdb086dd95ff439b5ad3226
p3nt4 <louis.nacfr@gmail.com>
2020-04-10 13:14:42 -0400

    Update setup.sh

--- Server/setup.sh
+++ Server/setup.sh
@@ -2,8 +2,18 @@
 command -v npm >/dev/null 2>&1 || { echo >&2 "npm required.  Aborting."; exit 1; }
 command -v node >/dev/null 2>&1 || { echo >&2 "node required.  Aborting."; exit 1; }
 
-printf "\n\nInstalling local npm dependencies\n";
-npm install 2>&1 >/dev/null;
+printf "Installing local npm dependencies...\n";
+npm install 2>&1 > /dev/null;
+
+if [ $? -eq 0 ]; then
+    echo "Done!"
+else
+    echo "FAILED !"
+    exit 1
+fi
+
+printf "Adding support for gridfs...\n";
+cp src/mongodb.js.grid src/mongodb.js
 
 if [ $? -eq 0 ]; then
     echo "Done!"
