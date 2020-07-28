b150a523bce85adeaab9240e310410a88f61337b
p3nt4 <louis.nacfr@gmail.com>
2020-07-28 16:46:45 -0400

    Update before-create-tunnel.js

--- Server/src/hooks/before-create-tunnel.js
+++ Server/src/hooks/before-create-tunnel.js
@@ -26,7 +26,7 @@ module.exports = function (options = {}) {
 
     data.implantId = context.data.implantId;
 
-    data.timeout = context.data.timeout? parseInt(context.data.timeout) : null;
+    data.timeout = parseInt(context.data.timeout) ? parseInt(context.data.timeout) : 30000;
 
     data.jobOptions = context.data.jobOptions ? context.data.jobOptions : {};
 
