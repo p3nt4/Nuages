76195026e362f00f7697622bc0ece53caff7bbaa
p3nt4 <xp3nt4@gmail.com>
2022-08-19 12:49:31 -0400

    Update before-create-implant-register.js

--- Server/src/hooks/before-create-implant-register.js
+++ Server/src/hooks/before-create-implant-register.js
@@ -71,7 +71,7 @@ module.exports = function (options = {}) {
         var message = webhook.customMessage !== "" ? webhook.customMessage : "### New Implant! :fire:" ;
         instance.post(webhook.url, {
           username:'New Implant Bot',
-          text: message + " \n| Hostname | Username | IP Address |\n|:---------|:--------:|:-----------|\n|"+data.hostname+"|"+data.username+"|"+data.localIp+"|\n"
+          text: `${message} \n| Hostname | Username | IP Address |\n|:---------|:--------:|:-----------|\n|${data.hostname}|${data.username}|${data.localIp}|\n`
         })
         .catch(error => {
           console.error(error);
